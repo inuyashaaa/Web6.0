742461ee0f94ab7d183131a766b7bcbc2da0aa42
inuyashaaa <huymanhtmhp@gmail.com>
2017-03-12 22:11:20 +0700

    Update Ship

--- js/main.js
+++ js/main.js
@@ -1,18 +1,36 @@
 var Nakama = {};
-Nakama.configs = {};
+Nakama.configs = {
+    GAME_WIDTH: 640,
+    GAME_HIGHT: 960,
+    MIN_WIDTH: 320,
+    MIN_HEIGHT: 480,
+    MAX_WIDth: 640,
+    MAX_HEIGHT: 960,
+    PLAYER1_POS : {
+      x: 200,
+      y:200
+    },
+    PLAYER2_POS : {
+      x: 400,
+      y: 200
+    }
+};
 
 window.onload = function() {
-    Nakama.game = new Phaser.Game(640, 960, Phaser.AUTO, '', {
-        preload: preload,
-        create: create,
-        update: update,
-        render: render
-    }, false, false);
+    Nakama.game = new Phaser.Game(
+        Nakama.configs.GAME_WIDTH,
+        Nakama.configs.GAME_HIGHT,
+        Phaser.AUTO, '', {
+            preload: preload,
+            create: create,
+            update: update,
+            render: render
+        }, false, false);
 }
 
 // preparations before game starts
 var preload = function() {
-    Nakama.game.scale.minWidth = 320;
+    Nakama.game.scale.minWidth = Nakama.configs.MIN_WIDTH;
     Nakama.game.scale.minHeight = 480;
     Nakama.game.scale.maxWidth = 640;
     Nakama.game.scale.maxHeight = 960;
@@ -30,32 +48,40 @@ var create = function() {
     Nakama.game.physics.startSystem(Phaser.Physics.ARCADE);
     Nakama.keyboard = Nakama.game.input.keyboard;
     Nakama.game.add.sprite(0, 0, 'background');
-    Nakama.player = Nakama.game.add.sprite(200, 200, 'assets', 'Spaceship1-Player.png');
+    Nakama.players = [];
+    Nakama.players.push(
+        new ShipController(
+            200,
+            200,
+            "Spaceship1-Player.png", {
+                up: Phaser.Keyboard.UP,
+                down: Phaser.Keyboard.DOWN,
+                left: Phaser.Keyboard.LEFT,
+                right: Phaser.Keyboard.RIGHT,
+                fire: Phaser.Keyboard.SPACEBAR
+            }
+        )
+    );
+    Nakama.players.push(
+        new ShipController(
+            400,
+            200,
+            "Spaceship2-Player.png", {
+                up: Phaser.Keyboard.W,
+                down: Phaser.Keyboard.S,
+                left: Phaser.Keyboard.A,
+                right: Phaser.Keyboard.D,
+                fire: Phaser.Keyboard.F
+            }
+        )
+    );
 
 }
-
 // update game state each frame
 var update = function() {
-
-    if (Nakama.keyboard.isDown(Phaser.Keyboard.UP)) {
-        if (Nakama.player.position.y - 10 >= 0) {
-            Nakama.player.position.y -= 10;
-        }
-    } else if (Nakama.keyboard.isDown(Phaser.Keyboard.DOWN)) {
-        if (Nakama.player.position.y + 10 <= (960 - 78)) {
-            Nakama.player.position.y += 10;
-        }
-    }
-    if (Nakama.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-        if (Nakama.player.position.x - 10 >= 0) {
-            Nakama.player.position.x -= 10;
-        }
-    } else if (Nakama.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-        if (Nakama.player.position.x + 10 <= (640 - 78)) {
-            Nakama.player.position.x += 10;
-        }
-    }
-
+    Nakama.players.forEach(function(ship) {
+        ship.update();
+    });
 }
 
 // before camera render (mostly for debug)
